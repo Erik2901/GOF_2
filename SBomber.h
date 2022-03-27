3d7ebafe6b62674f59adec2bc3dfb95e02a9b22d
Erik2901 <erik2901@mail.ru>
2022-03-27 21:59:09 +0400

    changes

--- SBomber.h
+++ SBomber.h
@@ -8,6 +8,59 @@
 #include "Ground.h"
 #include "Tank.h"
 
+
+/*
+class DropBomb {
+    Plane* pPlane;
+    DynamicObject* dobj;
+    uint16_t* bombsNumber;
+public:
+    DropBomb()
+};
+*/
+
+class Command {
+public:
+    virtual ~Command() {}
+    virtual void Execute() = 0;
+};
+
+class DeleteDynamicObj : public Command {
+    DynamicObject* dobj;
+    std::vector<DynamicObject*>& vecDynamicObj_;
+public:
+    DeleteDynamicObj(DynamicObject* dobj_, std::vector<DynamicObject*>& _vecDynamicObj) : dobj(dobj_), vecDynamicObj_(_vecDynamicObj) {}
+    void Execute() override;
+};
+
+class DeleteStaticObj : public Command {
+    DestroyableGroundObject* dobj;
+    std::vector<GameObject*>& vecStaticObj_;
+public:
+    DeleteStaticObj(DestroyableGroundObject* dobj_, std::vector<GameObject*>& _vecStaticObj) : dobj(dobj_), vecStaticObj_(_vecStaticObj) {}
+    void Execute() override;
+};
+
+class DropBomb : public Command {
+    Plane* pPlane;
+    std::vector<DynamicObject*>& vecDynamicObj_;
+    uint16_t& bombsNumber;
+    int16_t& score;
+public:
+    DropBomb(Plane* pPlane_, std::vector<DynamicObject*>& _vecDynamicObj, uint16_t& bombsNumber_ , int16_t&score_) : pPlane(pPlane_), vecDynamicObj_(_vecDynamicObj), bombsNumber(bombsNumber_), score(score_){}
+    void Execute() override;
+};
+
+class DropDecBomb : public Command {
+    Plane* pPlane;
+    std::vector<DynamicObject*>& vecDynamicObj_;
+    uint16_t& bombsNumber;
+    int16_t& score;
+public:
+    DropDecBomb(Plane* pPlane_, std::vector<DynamicObject*>& _vecDynamicObj, uint16_t& bombsNumber_, int16_t& score_) : pPlane(pPlane_), vecDynamicObj_(_vecDynamicObj), bombsNumber(bombsNumber_), score(score_) {}
+    void Execute() override;
+};
+
 class SBomber
 {
 public:
@@ -24,15 +77,18 @@ public:
     void DrawFrame();
     void MoveObjects();
     void CheckObjects();
+    void CommandExecuter(Command* pCommand) {
+        pCommand->Execute();
+    }
 
 private:
-
+    //Command* pcommand;
     void CheckPlaneAndLevelGUI();
     void CheckBombsAndGround();
     void __fastcall CheckDestoyableObjects(Bomb* pBomb);
 
-    void __fastcall DeleteDynamicObj(DynamicObject * pBomb);
-    void __fastcall DeleteStaticObj(GameObject* pObj);
+    //void __fastcall DeleteDynamicObj(DynamicObject * pBomb);
+    //void __fastcall DeleteStaticObj(GameObject* pObj);
 
     Ground * FindGround() const;
     Plane * FindPlane() const;
@@ -40,7 +96,7 @@ private:
     std::vector<DestroyableGroundObject*> FindDestoyableGroundObjects() const;
     std::vector<Bomb*> FindAllBombs() const;
 
-    void DropBomb();
+    //void DropBomb();
 
     std::vector<DynamicObject*> vecDynamicObj;
     std::vector<GameObject*> vecStaticObj;
